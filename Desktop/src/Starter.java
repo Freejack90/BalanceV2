79d6f4e1be5d029a560643c3b249cc96430057c9
Feax <feax@mail.ru>
2013-01-14 11:39:41 +0900

    DemoCommit

--- Desktop/src/Starter.java
+++ Desktop/src/Starter.java
@@ -2,13 +2,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import main.AndroidGame;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Freejack
- * Date: 14.01.13
- * Time: 10:38
- * To change this template use File | Settings | File Templates.
- */
 public class Starter {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
@@ -17,5 +10,6 @@ public class Starter {
         cfg.height = 800;
         cfg.useGL20 = false;
         new LwjglApplication(new AndroidGame(), cfg);
+        //Git
     }
 }
